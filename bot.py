0b6f83fd99a35816a0c080e0bee47387a0e43d78
DiogoZMartini <diogo.81226211@gmail.com>
2023-10-19 13:03:42 -0300

    Alteração no bot.py e talks.py

--- bot.py
+++ bot.py
@@ -1,6 +1,19 @@
+import asyncio
 import discord
 from discord.ext import commands
+import os
+intents = discord.Intents.default()
+intents. message_content = True
 
-bot = commands.Bot("!")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-bot.run('Token')
+async def load():    
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f'commands.{filename[:-3]}')
+
+async def main():
+    await load()    
+    await bot.start('')
+
+asyncio.run(main())
--- commands/talks.py
+++ commands/talks.py
@@ -1,8 +1,24 @@
+import discord
 from discord.ext import commands
 
 class Talks(commands.Cog):
+    
     def __init__(self, bot):
         self.bot = bot
 
-def setup(bot):
-    bot.add_Cog(Talks(bot))
+    @commands.command(name="ajuda")
+    async def send_help(self, ctx):
+        try:
+            await ctx.author.send(f':notebook_with_decorative_cover: ***Lista de Comandos*** :notebook_with_decorative_cover:\n'+
+                '***!roll d20*** --> :game_die: Rola um dado de 20 lados :game_die:\n'+
+                '**!ap1** Mostra uma Imagme do Primeiro Apostolo\n'+
+                '**!ap2** Mostra uma Imagme do Segundo Apostolo\n'+
+                '**!ap3** Mostra uma Imagme do Trceiro Apostolo\n'+
+                '**!ap4** Mostra uma Imagme do Quarto Apostolo\n'+
+                '**!ap5** Mostra uma Imagme do Quinto Apostolo\n'+
+                '**!apc5** Mostra uma Imagme do companheiro do Quinto Apostolo')
+        except discord.errors.Forbidden:
+            await ctx.send('Não foi possivel enviar a mensagem, habilite receber mensagens de qualquer pessoa do servidor (Opções > Privacidade)')
+    
+async def setup(bot):
+    await bot.add_cog(Talks(bot))
